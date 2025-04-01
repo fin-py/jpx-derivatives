76d66174f4f48a1d9d4dd94c0bf1ace1ba66517a
takei <takeiy0723@gmail.com>
2025-04-01 14:27:45 +0900

    weeklyの限週にW2を付与、testcode作成

--- src/jpx_derivatives/check_maturity.py
+++ src/jpx_derivatives/check_maturity.py
@@ -1,9 +1,9 @@
 import datetime
+
 import pandas as pd
 
 
 class maturity_info_class:
-
     def __init__(self, sq_data: pd.DataFrame):
         """
         sq_data: special_quotation.parquet
@@ -44,13 +44,13 @@ class maturity_info_class:
     def check_option_maturity(
         self,
         dt: datetime.datetime,
-        maturity_n: int,
+        nth_contract_month: int,
         contract_frequency: str,
     ) -> tuple[datetime.datetime, datetime.datetime, str]:
         """
-        渡されたdtの第[maturity_n]限月の最終取引日時とSQ日を返す。
+        渡されたdtの第[nth_contract_month]限月の最終取引日時とSQ日を返す。
         dt: 基準日時
-        maturity_n: 2の場合第2限月を返す、マイナスも可
+        nth_contract_month: 2の場合第2限月を返す、マイナスも可
         contract_frequency: "monthly" / "weekly"
         return: lasttradingday, sqdate, contractmonth
         contractmonthは "2025-03", "2025-03-W5" など
@@ -59,25 +59,33 @@ class maturity_info_class:
         if dt.tzinfo is None:
             jst = datetime.timezone(datetime.timedelta(hours=9))
             dt = dt.replace(tzinfo=jst)
-        if maturity_n == 0:
-            raise ValueError(f"maturity_nは0以外で指定してください。（マイナスも可）")
+        if nth_contract_month == 0:
+            raise ValueError(
+                "nth_contract_monthは0以外で指定してください。（マイナスも可）"
+            )
         # if dt < self.sq_data["LastTradingDay"].min():
         #    raise ValueError(f"dt={self.sq_data['LastTradingDay'].min()}以降のみ対応")
         if dt > self.sq_data["LastTradingDay"].max():
             raise ValueError(f"dt={self.sq_data['LastTradingDay'].max()}以前のみ対応")
         if contract_frequency not in ["monthly", "weekly"]:
             raise ValueError(
-                f"contract_frequencyは'monthly' / 'weekly'のみ指定してください。"
+                "contract_frequencyは'monthly' / 'weekly'のみ指定してください。"
             )
 
         # monthly / weekly のフィルタ
         if contract_frequency == "monthly":
-            sq_data = self.sq_data[~self.sq_data["ContractMonth"].str.contains("W")]
+            sq_data = self.sq_data[
+                ~self.sq_data["ContractMonth"].str.contains("W")
+            ].copy()
         else:
             # weeklyの場合は全て対象
-            sq_data = self.sq_data
+            sq_data = self.sq_data.copy()
+            # weeklyの場合第２週にWを付ける
+            sq_data["ContractMonth"] = sq_data["ContractMonth"].apply(
+                lambda x: f"{x}-W2" if "W" not in x else x
+            )
 
-        if 0 < maturity_n:
+        if 0 < nth_contract_month:
             sq_data = sq_data[sq_data["LastTradingDay"] > dt]
         else:
             # マイナス限月の場合逆順にする
@@ -85,10 +93,10 @@ class maturity_info_class:
                 "LastTradingDay", ascending=False
             )
 
-        if sq_data.shape[0] < abs(maturity_n):
-            raise ValueError(f"{dt}の第{maturity_n}限月は存在しません")
+        if sq_data.shape[0] < abs(nth_contract_month):
+            raise ValueError(f"{dt}の第{nth_contract_month}限月は存在しません")
 
-        lasttradingday = sq_data["LastTradingDay"].iloc[abs(maturity_n) - 1]
-        sqdate = sq_data["SpecialQuotationDay"].iloc[abs(maturity_n) - 1]
-        contractmonth = sq_data["ContractMonth"].iloc[abs(maturity_n) - 1]
+        lasttradingday = sq_data["LastTradingDay"].iloc[abs(nth_contract_month) - 1]
+        sqdate = sq_data["SpecialQuotationDay"].iloc[abs(nth_contract_month) - 1]
+        contractmonth = sq_data["ContractMonth"].iloc[abs(nth_contract_month) - 1]
         return lasttradingday, sqdate, contractmonth
